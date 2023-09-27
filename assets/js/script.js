1160d392d81af37b46c99b920dd4c2b22604b5a3
henrikandersson4 <>
2023-09-27 09:13:02 +0000

    Added variables to store elements an game state

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Variables to store elements and game state
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+let highScore = localStorage.getItem("high-score") || 0;
